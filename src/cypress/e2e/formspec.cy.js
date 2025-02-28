bd394efeb4ff3c6786798f41878f034e3fa624b3
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-27 20:33:09 -0800

    initial update

---
+++ src/cypress/e2e/formspec.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Submits a rating", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-undef
+    cy.visit("http://localhost:3001/");
+  });
+
+  it("Submits a rating", () =>{
+    
+  })
+});
